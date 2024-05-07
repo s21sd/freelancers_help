cd791e3dcb755df3035e61b08e1a2feb9130ac49
s21sd <srivastavasunny359@gmail.com>
2024-05-07 20:15:32 +0530

    New Commit

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,7 +7,7 @@ export async function middleware(request: NextRequest) {
     if (data.user === null) {
         return NextResponse.redirect(new URL("/login?error=Please login first to access this route ", request.url))
     }
-    return NextResponse.next()
+    return NextResponse.next();
 }
 export const config = {
     matcher: [],
