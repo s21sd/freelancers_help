207a2b24a1910a70a808bb0d95b13792eff1e173
s21sd <srivastavasunny359@gmail.com>
2024-05-17 02:51:10 +0530

    d

--- src/actions/authActions.ts
+++ src/actions/authActions.ts
@@ -3,10 +3,11 @@ import { RegisterValidator } from '@/validations/authSchema'
 import vine, { errors } from '@vinejs/vine'
 import { createClient } from '@/lib/supabase/supabaseServer'
 import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
 export async function registerAction(prevState: any, formdata: FormData) {
     const supabase = createClient(cookies());
     try {
-        console.log("The form data is ", formdata)
+       
         const data = {
             name: formdata.get("name"),
             username: formdata.get("username"),
@@ -15,11 +16,44 @@ export async function registerAction(prevState: any, formdata: FormData) {
             password_confirmation: formdata.get("password_confirmation"),
         }
         const payload = await RegisterValidator.validate(data)
-        
+
         // Checking username if exists
-        const { data: userdata, error } = await supabase.from("auth").select("id").eq("username", payload.username)
+        const { data: userdata, error } = await supabase.from("Auth").select("id").eq("username", payload.username)
         console.log("The user data ", userdata)
         console.log("The error ", error)
+        if (userdata && userdata?.length > 0) {
+            return {
+                status: 400,
+                errors: {
+                    username: "Username is already used"
+                }
+            }
+        }
+
+        const { error: signUpErr } = await supabase.auth.signUp({
+            email: payload.email,
+            password: payload.password,
+            options: {
+                data: {
+                    name: payload.name,
+                    username: payload.username
+                }
+            }
+
+        })
+        // Now After Sign up Going to sign in direclty
+        await supabase.auth.signInWithPassword({
+            email: payload.email,
+            password: payload.password
+        })
+        if (signUpErr) {
+            return {
+                status: 400,
+                error: {
+                    email: signUpErr.message
+                }
+            }
+        }
 
     } catch (error) {
         if (error instanceof errors.E_VALIDATION_ERROR) {
@@ -27,5 +61,6 @@ export async function registerAction(prevState: any, formdata: FormData) {
             return { status: 400, error: error.messages }
         }
     }
+    return redirect("/")
 
 }
